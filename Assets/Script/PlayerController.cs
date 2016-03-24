61e448bed9f6c70a1de193bef56eabd8a923ca20
Paraya <paradrops.3636@gmail.com>
2016-03-24 19:44:01 +0900

    initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 10;
+
+	void FixedUpdate ()
+	{
+		float x =  Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// rigidbodyのx軸（横）とz軸（奥）に力を加える
+		rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+}
